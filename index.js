78d941166d745eca2a874a347985a7869eea456f
Maxim Klochkov <m.klochkov@sap.com>
2020-06-17 20:16:36 +0300

    Adding Timer class

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('[ STARTED ]');
+console.log('[ SCRIPT STARTED ]');
 
 // STEP BY STEP APPROACH
 // =====================
@@ -37,3 +37,43 @@ console.log('[ STARTED ]');
 // pause()
 // tick()
 // onDurationChange()
+
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    }
+
+    start = () => {
+        this.tick();
+        this.interval = setInterval(this.tick, 1000);
+    }
+
+    pause = () => {
+        clearInterval(this.interval);
+    }
+
+    tick = () => {
+        this.timeRemaining = this.timeRemaining - 1 ;
+    }
+
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value);
+    }
+
+    set timeRemaining(time) {
+        this.durationInput.value = time;
+    }
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
+
+// timer.start();
